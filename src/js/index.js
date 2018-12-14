9a218fa15f8730b9471a0cec0e84f649d4fd54d9
Bryan Leanoe <bryan.leano@gmail.com>
2018-12-13 19:06:42 -0700

    add Search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const key = 'f8f272e31335f175df5ccfae83b4cbe0';
-  try {
-    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error)
-  }
-  
-}
-getResults('tomato pasta');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
