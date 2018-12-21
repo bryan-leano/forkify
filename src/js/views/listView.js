fc3b0fe16b173e59c0d1ab4d0b0e10f32e08b58c
Bryan Leanoe <bryan.leano@gmail.com>
2018-12-21 13:39:06 -0700

    built shopping list controller

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -4,10 +4,10 @@ export const renderItem = item => {
   const markup = `
     <li class="shopping__item" data-itemid=${item.id}>
       <div class="shopping__count">
-        <input type="number" value="${item.count}" step="${item.count}">
-        <p>g</p>
+        <input type="number" value="${item.count}" step="${item.count}" class="shopping__count-value">
+        <p>${item.unit}</p>
       </div>
-      <p class="shopping__description">Pasta</p>
+      <p class="shopping__description">${item.ingredient}</p>
       <button class="shopping__delete btn-tiny">
         <svg>
           <use href="img/icons.svg#icon-circle-with-cross"></use>
@@ -15,8 +15,10 @@ export const renderItem = item => {
       </button>
     </li>
   `;
+  elements.shopping.insertAdjacentHTML('beforeend', markup);
 };
 
 export const deleteItem = id => {
-  
+  const item = document.querySelector(`[data-itemid="${id}"]`);
+  if (item) item.parentElement.removeChild(item);
 };
